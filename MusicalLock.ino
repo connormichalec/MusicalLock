299620bbdbe58789045790f179db1e2c6b76ce35
realamoghm <43323507+realamoghm@users.noreply.github.com>
2022-10-29 15:28:53 -0500

    Added notes and frequencies
    Added notes and their respective frequencies to the Arduino file.

--- MusicalLock.ino
+++ MusicalLock.ino
@@ -3,5 +3,22 @@ void main() {
 }
 
 void loop() {
+    /*
+    Notes and frequencies:
+    C - 261.63 Hz
+    C#- 277.18 Hz
+    D - 293.66 Hz
+    D# - 311.13 Hz
+    E - 329.63 Hz
+    F - 349.23 Hz
+    F# - 369.99 Hz
+    G - 392.00 Hz
+    G#- 415.30 Hz
+    A - 440.00 Hz
+    A# - 466.16 Hz
+    B- 493.88 Hz
+
+    Source: https://pages.mtu.edu/~suits/notefreqs.html
+    */
     
 }
